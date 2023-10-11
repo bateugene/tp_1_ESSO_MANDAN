d729c9483f125fe101dff8bb2aea6b668d52d0b1
unknown <essosoligue@gmail.com>
2023-10-10 22:22:05 -0400

    the end

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title> Validation du mot de passe</title>
+</head>
+<body>
+    <form action="validate_password.php"method="POST">
+         <label for="password">Mot de passe:</label>
+         <input type="password" id="password"name="password">
+         <input type="submit" value="Valider">
+    </form>
+</body>
+</html>
